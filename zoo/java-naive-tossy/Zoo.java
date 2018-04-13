5bba98e7e77999d3ee9667afdf71c3c62431865e
Hiroki Sato <hsugar.310@gmail.com>
2018-04-13 17:58:58 +0900

    Reformat java-naive code

--- zoo/java-naive-tossy/Zoo.java
+++ zoo/java-naive-tossy/Zoo.java
@@ -10,8 +10,8 @@ public class Zoo {
     // Return true if str appears in arr[0:n)
     // Otherwise, return false
     private static boolean checkAppearance(final String arr[], final int n, final String str) {
-        for(int i=0; i<n; i++) {
-            if(arr[i].equals(str)) return true;
+        for (int i = 0; i < n; i++) {
+            if (arr[i].equals(str)) return true;
         }
         return false;
     }
@@ -19,18 +19,18 @@ public class Zoo {
     private static void solve() {
         final int n = sc.nextInt();
         String knownAnimals[] = new String[n];
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             knownAnimals[i] = sc.next();
         }
 
         final int m = sc.nextInt();
         String checkAnimals[] = new String[m];
-        for (int i=0; i<m; i++) {
+        for (int i = 0; i < m; i++) {
             checkAnimals[i] = sc.next();
 
-            if(checkAppearance(knownAnimals, n, checkAnimals[i])) {
+            if (checkAppearance(knownAnimals, n, checkAnimals[i])) {
                 // known
-                if(checkAppearance(checkAnimals, i, checkAnimals[i])) {
+                if (checkAppearance(checkAnimals, i, checkAnimals[i])) {
                     System.out.println(RESULT_SEEN);
                 } else {
                     System.out.println(RESULT_FIRST);
