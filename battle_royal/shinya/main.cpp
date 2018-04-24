7ee97eab1c73afd79acb8ac21ba4d3da9c0865c7
ShinyaKato <k.shin220@gmail.com>
2018-04-25 08:11:01 +0900

    Modify

--- battle_royal/shinya/main.cpp
+++ battle_royal/shinya/main.cpp
@@ -3,17 +3,21 @@ using namespace std;
 
 int main(void) {
   const int N = 100;
+  int T;
   int K[N];
 
-  for(int i = 0; i < N; i++) {
-    cin >> K[i];
-  }
+  cin >> T;
+  for(int t = 0; t < T; t++) {
+    for(int i = 0; i < N; i++) {
+      cin >> K[i];
+    }
 
-  int sum = 0;
-  for(int i = 0; i < N; i++) {
-    sum += K[i];
+    int sum = 0;
+    for(int i = 0; i < N; i++) {
+      sum += K[i];
+    }
+    cout << N - sum << endl;
   }
-  cout << N - sum << endl;
 
   return 0;
 }
