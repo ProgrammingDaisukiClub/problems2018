bf4ca45d4fca80bf6e64c2e8251fb1f5f9adf5e2
ryoissy <issyw116@gmail.com>
2018-04-24 22:42:48 +0900

    指摘された箇所の修正と問題文の追加をしました

--- tsunahiki/tests/generator.cpp
+++ tsunahiki/tests/generator.cpp
@@ -5,20 +5,25 @@
 
 using namespace std;
 
+int a[2][101];
+
 void generate(const string &file_name, int num_case, int max_num) {
   ofstream ofs(file_name);
   ofs << num_case <<endl;
 
   for (int i=0; i<num_case; i++) {
-    int N=rnd.next(MIN_AB,max_num);
+    int N=rnd.next(MIN_ABN,max_num);
     ofs << N << endl;
     for(int k=0;k<2;k++){
-      int ele=1;
-      for(int j=0;j<N;j++){
-        if(j!=0)ofs << " ";
-        ele=rnd.next(ele,max_num);
-      }
-      ofs << endl;
+    	for(int j=0;j<N;j++){
+    		a[k][j]=rnd.next(MIN_ABN,MAX_ABN);
+    	}
+    	sort(a[k],a[k]+N);
+    	for(int j=0;j<N;j++){
+    		if(j!=0)ofs << " ";
+    		ofs << a[k][j];
+    	}
+    	ofs << endl;
     }
   }
 }
@@ -27,9 +32,9 @@ int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
 
   generate("small.in", 10, 10);
-  generate("large.in", MAX_T, MAX_AB);
+  generate("large.in", MAX_T, MAX_ABN);
   for (int i=0; i<10; i++) {
-    generate("random_test" + to_string(i) + ".in", MAX_T, MAX_AB);
+    generate("random_test" + to_string(i) + ".in", MAX_T, MAX_ABN);
   }
 
   return 0;
