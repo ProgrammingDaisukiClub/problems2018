dce5743727888db467c518b8866f4dbcb68d8129
DAyama <taiki_yamada@akane.waseda.jp>
2018-05-06 12:31:10 +0900

    add validator

--- even2primes/tests/validator.cpp
+++ even2primes/tests/validator.cpp
@@ -4,10 +4,22 @@
 using namespace std;
 
 void check_case(){
-  
+  int E = inf.readInt(2, MAX_E, "E");
+  inf.readEoln();
+
+  ensure(E % 2 == 0);
 }
 
 int main(){
+  registerValidation();
 
+  int cases = inf.readInt(1, MAX_T, "cases");
+  inf.readEoln();
+
+  for(int i=0; i<cases; ++i){
+    check_case();
+  }
+
+  inf.readEof();
   return 0;
 }
