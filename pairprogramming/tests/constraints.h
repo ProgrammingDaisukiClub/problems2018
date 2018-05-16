7075b47943e680da901219135f929d392556ec3d
rdrgn <rdrgn@users.noreply.github.com>
2018-05-17 01:27:12 +0900

    update test cases

--- pairprogramming/tests/constraints.h
+++ pairprogramming/tests/constraints.h
@@ -1,3 +1,3 @@
-#define MAX_T (100)
+#define MAX_T (10)
 #define MAX_N (1e3)
 #define MAX_A (1e3)
--- pairprogramming/tests/generator.cpp
+++ pairprogramming/tests/generator.cpp
@@ -4,11 +4,20 @@
 
 using namespace std;
 
-void generate(const string& file_name, int num_case, int n, int max_a) {
+void generate(const string& file_name, int num_case, int max_n, int max_a) {
   ofstream ofs(file_name);
   ofs << num_case << endl;
 
   for (int i = 0; i < num_case; i++) {
+    int n;
+    if (i == 0) {
+      n = max_n;
+    } else if (i == 1) {
+      n = 1;
+    } else {
+      n = rnd.next(1, max_n);
+    }
+
     ofs << n << endl;
 
     for (int i = 0; i < n; i++) {
@@ -21,7 +30,7 @@ void generate(const string& file_name, int num_case, int n, int max_a) {
 int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
 
-  generate("small.in", 10, 10, 10);
+  generate("small.in", MAX_T, 10, 10);
   generate("large.in", MAX_T, MAX_N, MAX_A);
   for (int i = 0; i < 10; i++) {
     generate("random_test" + to_string(i) + ".in", MAX_T, MAX_N, MAX_A);
